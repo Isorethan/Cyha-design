d07de957e62b98cd6cb4078fa100b43e0c654d73
Isorethan <dessout.yannick@gmail.com>
2020-03-13 14:56:11 +0100

    finalité logique select

--- src/views/Blog.js
+++ src/views/Blog.js
@@ -189,7 +189,7 @@ chooseCategorie = (e) => {
         <section id="actualite-content">
         <div id="nav-articles">
                 <select id="nav-list"  onChange={this.chooseCategorie.bind(this)} >
-                  <option className="nav-list-item" onClick={this.resetCat}>TOUS</option>
+               
                   { (allCategories) && allCategories.map((cat,i)=>
                   <option className="nav-list-item" value={cat.id} key={i}  >{cat.name}</option>
                   )}
