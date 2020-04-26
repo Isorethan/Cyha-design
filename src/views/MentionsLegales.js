5c2290804d9513ea6debf260cda83df6f322733e
Obojinld <[mariesan.ledu@outlook.com]>
2020-04-26 15:00:56 +0200

    mentionlegales

--- src/views/MentionsLegales.js
+++ src/views/MentionsLegales.js
@@ -12,7 +12,7 @@ export default function MentionsLegales() {
 <p>Merci de lire avec attention les différentes modalités d’utilisation du présent site avant d’y parcourir ses pages. En vous connectant sur ce site, vous acceptez sans réserves les présentes modalités. Aussi, conformément à l’article n°6 de la Loi n°2004-575 du 21 Juin 2004 pour la confiance dans l’économie numérique, les responsables du présent site internet <a href="http://www.cyha-design.fr">www.cyha-design.fr</a> sont :</p>
 
 <p>
-    <h3>Editeur du Site :</h3><br/>
+    <h3>Editeur du Site :</h3>
     Haidara Cherif <br/>
     Numéro de SIRET :  803 021 252 00033<br/>
     Responsable editorial : Haidara Cherif<br/>
@@ -31,7 +31,7 @@ export default function MentionsLegales() {
 
 
 <p>
-    <h3>Développement :</h3> <br/>
+    <h3>Développement :</h3> 
     Marie LE DU  et Yannick DESSOUT , stagiaires . <br/>
     Adresse : 77 Avenue Aristide Briand, 35000 Rennes <br/>
     Site Web : <a href="http://www.cyha-design.fr">www.cyha-design.fr</a>
@@ -39,7 +39,7 @@ export default function MentionsLegales() {
 
 
 <p>
-    <h3> Conditions d’utilisation : </h3> <br/>
+    <h3> Conditions d’utilisation : </h3>
     Ce site (<a href="http://www.cyha-design.fr">www.cyha-design.fr</a>) est proposé en différents langages web (HTML, HTML5, Javascript, CSS, etc…) pour un meilleur confort d'utilisation et un graphisme plus agréable, nous vous recommandons de recourir à des navigateurs modernes comme Internet explorer, Safari, Firefox, Google Chrome, etc…
     Les mentions légales ont été générées sur le site <a title="générateur de mentions légales pour site internet gratuit" href="http://www.generateur-de-mentions-legales.com">Générateur de mentions légales</a>, offert par <a title="imprimerie paris, imprimeur paris" href="http://welye.com">Welye</a>.
 </p>
@@ -50,7 +50,7 @@ export default function MentionsLegales() {
 </p>
 
 <p>
-<h3>Cookies :</h3> <br/>
+<h3>Cookies :</h3> 
 Le site <a href="http://www.cyha-design.fr">www.cyha-design.fr</a>  
 peut-être amené à vous demander l’acceptation des cookies pour des besoins de statistiques et d'affichage. 
 Un cookies est une information déposée sur votre disque dur par le serveur du site que vous visitez. 
@@ -75,14 +75,14 @@ Les utilisateurs, les abonnés et les visiteurs des sites internet de ne peuvent
 Dans l'hypothèse où un utilisateur ou visiteur souhaiterait mettre en place un hyperlien en direction d’un des sites internet de Haidara Cherif, il lui appartiendra d'adresser un email accessible sur le site afin de formuler sa demande de mise en place d'un hyperlien. Haidara Cherif se réserve le droit d’accepter ou de refuser un hyperlien sans avoir à en justifier sa décision.
 </p>
 
-
-<p> <h3> Services fournis : </h3> <br/>
+<h3> Services fournis : </h3> 
+<p> 
 L'ensemble des activités de la société ainsi que ses informations sont présentés sur notre site <a href="http://www.cyha-design.fr">www.cyha-design.fr</a>.<br/>
 Haidara Cherif s’efforce de fournir sur le site www.cyha-design.fr des informations aussi précises que possible. les renseignements figurant sur le site <a href="http://www.cyha-design.fr">www.cyha-design.fr</a> ne sont pas exhaustifs et les photos non contractuelles. Ils sont donnés sous réserve de modifications ayant été apportées depuis leur mise en ligne. Par ailleurs, tous les informations indiquées sur le site www.cyha-design.fr sont données à titre indicatif, et sont susceptibles de changer ou d’évoluer sans préavis. 
 </p>
 
-
-<p><h3>Limitation contractuelles sur les données : </h3><br/>
+<h3>Limitation contractuelles sur les données : </h3>
+<p>
 Les informations contenues sur ce site sont aussi précises que possible et le site remis à jour à différentes périodes de l’année, mais peut toutefois contenir des inexactitudes ou des omissions. Si vous constatez une lacune, erreur ou ce qui parait être un dysfonctionnement, merci de bien vouloir le signaler par email, à l’adresse haidaracherifart@hotmail.fr, en décrivant le problème de la manière la plus précise possible (page posant problème, type d’ordinateur et de navigateur utilisé, …). <br/>
 
 Tout contenu téléchargé se fait aux risques et périls de l'utilisateur et sous sa seule responsabilité. En conséquence, ne saurait être tenu responsable d'un quelconque dommage subi par l'ordinateur de l'utilisateur ou d'une quelconque perte de données consécutives au téléchargement. <span style={{color: "#323333"}}>De plus, l’utilisateur du site s’engage à accéder au site en utilisant un matériel récent, ne contenant pas de virus et avec un navigateur de dernière génération mis-à-jour</span> <br/>
@@ -90,21 +90,26 @@ Tout contenu téléchargé se fait aux risques et périls de l'utilisateur et so
 Les liens hypertextes mis en place dans le cadre du présent site internet en direction d'autres ressources présentes sur le réseau Internet ne sauraient engager la responsabilité de Haidara Cherif.
 </p>
 
-<p><h3>Propriété intellectuelle :</h3><br/>
+<h3>Propriété intellectuelle :</h3>
+<p>
 Tout le contenu du présent sur le site <a href="http://www.cyha-design.fr">www.cyha-design.fr</a>, incluant, de façon non limitative, les graphismes, images, textes, vidéos, animations, sons, logos, gifs et icônes ainsi que leur mise en forme sont la propriété exclusive de la société à l'exception des marques, logos ou contenus appartenant à d'autres sociétés partenaires ou auteurs. <br/>
 
 Toute reproduction, distribution, modification, adaptation, retransmission ou publication, même partielle, de ces différents éléments est strictement interdite sans l'accord exprès par écrit de Haidara Cherif. Cette représentation ou reproduction, par quelque procédé que ce soit, constitue une contrefaçon sanctionnée par les articles L.335-2 et suivants du Code de la propriété intellectuelle. Le non-respect de cette interdiction constitue une contrefaçon pouvant engager la responsabilité civile et pénale du contrefacteur. En outre, les propriétaires des Contenus copiés pourraient intenter une action en justice à votre encontre.
 </p>
 
-<p><h3>Déclaration à la CNIL :</h3> <br/>
+<h3>Déclaration à la CNIL :</h3> 
+<p>
 Conformément à la loi 78-17 du 6 janvier 1978 (modifiée par la loi 2004-801 du 6 août 2004 relative à la protection des personnes physiques à l'égard des traitements de données à caractère personnel) relative à l'informatique, aux fichiers et aux libertés, ce site n'a pas fait l'objet d'une déclaration  auprès de la Commission nationale de l'informatique et des libertés (<a href="http://www.cnil.fr/">www.cnil.fr</a>).
 </p>
 
-<p><h3>Litiges :</h3><br/>
+<h3>Litiges :</h3>
+<p>
 Les présentes conditions du site <a href="http://www.cyha-design.fr">www.cyha-design.fr</a> sont régies par les lois françaises et toute contestation ou litiges qui pourraient naître de l'interprétation ou de l'exécution de celles-ci seront de la compétence exclusive des tribunaux dont dépend le siège social de la société. La langue de référence, pour le règlement de contentieux éventuels, est le français.
 </p>
 
-<p> <h3>Données personnelles :</h3><br/>
+
+<h3>Données personnelles :</h3>
+<p>
 De manière générale, vous n’êtes pas tenu de nous communiquer vos données personnelles lorsque vous visitez notre site Internet <a href="http://www.cyha-design.fr">www.cyha-design.fr</a>. <br/>
 
 Cependant, ce principe comporte certaines exceptions. En effet, pour certains services proposés par notre site, vous pouvez être amenés à nous communiquer certaines données telles que : votre nom, votre fonction, le nom de votre société, votre adresse électronique, et votre numéro de téléphone. Tel est le cas lorsque vous remplissez le formulaire qui vous est proposé en ligne, dans la rubrique « contact ». Dans tous les cas, vous pouvez refuser de fournir vos données personnelles. Dans ce cas, vous ne pourrez pas utiliser les services du site, notamment celui de solliciter des renseignements sur notre société, ou de recevoir les lettres d’information. <br/>
